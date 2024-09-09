031cc901e4799241f04f60e8006628144aced5d6
Bianca Rosa <bianca@lumosidentity.com>
2024-09-09 20:31:47 -0300

    format strings in go

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ func main() {
 	fmt.Println("Hello, Go Essentials!") // String
 
 	fmt.Println(2) // Inteiros
+	fmt.Printf("Hello, I'm %v and I'm an integer\n", 2)
+	fmt.Printf("Hello, I'm %d and I'm an integer\n", 2)
 
 	fmt.Println(2 + 5) // Expressões
 
