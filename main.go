78c9902061817d6d670018fd1ceaef993715eeef
Bianca Rosa <bianca@lumosidentity.com>
2024-09-09 20:28:27 -0300

    add types of values in go

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go Essentials!")
+
+	// Olá, assim eu faço um Hello World em Go!
+	fmt.Println("Hello, Go Essentials!") // String
+
+	fmt.Println(2) // Inteiros
+
+	fmt.Println(2 + 5) // Expressões
+
+	// Booleanos
+	fmt.Println(true)
+	fmt.Println(false)
 }
