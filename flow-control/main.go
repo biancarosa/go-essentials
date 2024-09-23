a1e8134a4a8706b3e6d16a2fae02917213f6a03b
Bianca Rosa <bianca@lumosidentity.com>
2024-09-23 12:05:34 -0300

    add flow control

--- flow-control/main.go
+++ flow-control/main.go
@@ -5,14 +5,16 @@ import "fmt"
 const MAX int = 100
 
 func main() {
-	// expr := n < MAX // declarando expr e atribuindo o valor (n < MAX)
-	// if expr {
-	// 	fmt.Printf("%d é menor do que MAX == %d\n", n, MAX)
-	// } else {
-	// 	fmt.Printf("%d é maior do que MAX == %d\n", n, MAX)
-	// }
 
-	for n := 0; n < MAX; n = n + 1 {
+	n := 0
+	expr := n < MAX // declarando expr e atribuindo o valor (n < MAX)
+	if expr {
+		fmt.Printf("%d é menor do que MAX == %d\n", n, MAX)
+	} else {
+		fmt.Printf("%d é maior do que MAX == %d\n", n, MAX)
+	}
+
+	for n := 0; n < MAX; n++ {
 		if n%2 == 0 {
 			fmt.Printf("%d é par\n", n)
 		} else {
@@ -21,6 +23,28 @@ func main() {
 	}
 
 	// for (inicializacao); (condicao que precisa ser verdade); (incremento)
-	for i := 0; i < MAX; i++ {
+	for i := 0; i < MAX; i++ {}
+
+	for {
+		n = n+1;
+		fmt.Println(n)
+		if n > MAX {
+			break;
+		}
+	} // loop infinito (não existe while em Go, apenas for infinito)
+
+	switch n < MAX {
+	case true: {
+		fmt.Println("n é menor do que max")	
+	}	
+	case false: {
+		fmt.Println("n não é menor do que max")	
+	}	
 	}
+
+	switch n {
+	case 100: fmt.Println("hello")
+	default: fmt.Println("não é 100")
+	}
+
 }
