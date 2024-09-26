99e9e2efebd4f0b44aff08352896e40ef4162d6d
Bianca Rosa <bianca@lumosidentity.com>
2024-09-26 19:17:18 -0300

    add codigo de outras operações

--- calc/main.go
+++ calc/main.go
@@ -7,14 +7,34 @@ func sum(a int, b int) int {
 	return a + b
 }
 
-// func multiply() {}
-// func divide()   {}
-// func subtract() {}
+func multiply(a int, b int) int {
+	fmt.Printf("Multiplicando %d * %d\n", a, b)
+	return a * b
+}
+
+func divide(a int, b int) int {
+	fmt.Printf("Dividindo %d / %d\n", a, b)
+	return a / b
+}
+
+func subtract(a int, b int) int {
+	fmt.Printf("Subtraindo %d - %d\n", a, b)
+	return a - b
+}
 
 func main() {
 	fmt.Println("Calculadora #GoEssentials")
 
-	result := sum(5, 10)
+	result := sum(5, 10) // criação e atribuição
+	fmt.Println(result)
+
+	result = divide(10, 5) // só atribuindo
+	fmt.Println(result)
+
+	result = multiply(5, 100) // só atribuindo
+	fmt.Println(result)
+
+	result = subtract(50, 10) // só atribuindo
 	fmt.Println(result)
 
 }
