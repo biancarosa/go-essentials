3309278b37e7dc298d739360a2c82347694b80ee
Bianca Rosa <bianca@lumosidentity.com>
2024-09-26 19:54:07 -0300

    fim da aula de hoje

--- calc/main.go
+++ calc/main.go
@@ -1,24 +1,59 @@
 package main
 
 import (
-	"fmt"
-
 	"calculadora/operations"
+	"fmt"
+	"os"
+	"strconv"
 )
 
+// Coisas futuras legais de fazer:
+// 1 - Passar a tratar valores de ponto flutuante: go run . "*" 2.5 5
+// 2 - Multiplos (infinitos ou variáveis valores de entrada):
+// https://www.digitalocean.com/community/tutorials/how-to-use-variadic-functions-in-go
+// ---- mesmo programa sem modificação funcionaria para
+// ----- go run . "+" 1000 500 100 1000
+// ----- go run . "+" 1000 500 100 1000 100
+// ----- go run . "+" 1000 500 100 1000 100 20
+// 3 - Receber uma entrada mais bonitinha e tratar ela: go run . =100/10
 func main() {
-	fmt.Println("Calculadora #GoEssentials")
 
-	result := operations.Sum(5, 10) // criação e atribuição
-	fmt.Println(result)
+	// slice (array? maizomenos) uma lista de valores.
+	// args := os.Args
+	// fmt.Println(args)
 
-	result = operations.Divide(10, 5) // só atribuindo
-	fmt.Println(result)
+	op := os.Args[1]
 
-	result = operations.Multiply(5, 100) // só atribuindo
-	fmt.Println(result)
+	s1 := os.Args[2]
+	s2 := os.Args[3]
 
-	result = operations.Subtract(50, 10) // só atribuindo
-	fmt.Println(result)
+	n1, err := strconv.Atoi(s1)
+	if err != nil {
+		panic("Valor inválido")
+	}
+	n2, err := strconv.Atoi(s2)
+	if err != nil {
+		panic("Valor inválido")
+	}
+
+	fmt.Printf("Executando a operação %s com os valores %d e %d\n", op, n1, n2)
+	var result int
+	switch op {
+	case "+":
+		result = operations.Sum(n1, n2)
+	case "*":
+		result = operations.Multiply(n1, n2)
+	case "-":
+		result = operations.Subtract(n1, n2)
+	case "/":
+		result, err = operations.Divide(n1, n2)
+	}
+	// verificar se o erro existe antes de tentar usar o valor de result
+	if err != nil {
+		panic(err)
+		// panic para a execução do programa
+	}
+
+	fmt.Println("Resultado === ", result)
 
 }
--- calc/operations/divide.go
+++ calc/operations/divide.go
@@ -1,8 +1,34 @@
 package operations
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Divide(a int, b int) int {
+// Retorna 0 na divisão
+// func Divide(a int, b int) int {
+// 	fmt.Printf("Dividindo %d / %d\n", a, b)
+// 	if b == 0 {
+// 		fmt.Println("Não posso fazer divisão por zero!")
+// 		return 0
+// 	}
+// 	return a / b
+// }
+
+// panic e para a execução completamente
+// func Divide(a int, b int) int {
+// 	fmt.Printf("Dividindo %d / %d\n", a, b)
+// 	if b == 0 {
+// 		panic("Divisão por zero não permitida")
+// 	}
+// 	return a / b
+// }
+
+func Divide(a int, b int) (int, error) {
 	fmt.Printf("Dividindo %d / %d\n", a, b)
-	return a / b
+	if b == 0 {
+		err := errors.New("divisão por zero não permitida")
+		return 0, err
+	}
+	return a / b, nil
 }
